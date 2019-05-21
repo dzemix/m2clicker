44b0a687329a913d61b5e8ac4b4345fc04fe72c8
unknown <czesio517619032@wp.pl>
2019-05-21 15:10:04 +0200

    some changes in canvas

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,19 +2,16 @@
   <div class="main">
     <h2>M2 Clicker</h2>
     <my-canvas class="my-canvas">
-      <my-game></my-game>
     </my-canvas>
   </div>
 </template>
 
 <script>
 import MyCanvas from './myCanvas.vue'
-import myGame from './myGame.vue'
 export default {
   name: 'app',
   components: {
-    MyCanvas,
-    myGame
+    MyCanvas
   }
 }
 </script>
@@ -31,7 +28,5 @@ html, body {
   padding: 20px;
   box-sizing: border-box;
 }
-.my-canvas {
-  width: 1200px; height: 600px; border:1px solid black;
-}
+
 </style>
--- src/components/myCanvas.vue
+++ src/components/myCanvas.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-canvas-wrapper">
-    <canvas ref="my-canvas"></canvas>
+    <canvas ref="my-canvas" class="canvas" ></canvas>
     <slot></slot>
   </div>
 </template>
@@ -9,31 +9,20 @@
 export default {
   data () {
     return {
-      // By creating the provider in the data property, it becomes reactive,
-      // so child components will update when `context` changes.
-      provider: {
-        // This is the CanvasRenderingContext that children will draw to.
-        context: null
-      }
     }
   },
-
-  // Allows any child component to `inject: ['provider']` and have access to it.
-  provide () {
-    return {
-      provider: this.provider
-    }
-  },
-
   mounted () {
-    // We can't access the rendering context until the canvas is mounted to the DOM.
-    // Once we have it, provide it to all child components.
-    this.provider.context = this.$refs['my-canvas'].getContext('2d')
-
-    // Resize the canvas to fit its parent's width.
-    // Normally you'd use a more flexible resize system.
-    this.$refs['my-canvas'].width = this.$refs['my-canvas'].parentElement.clientWidth
-    this.$refs['my-canvas'].height = this.$refs['my-canvas'].parentElement.clientHeight
+    this.$refs['my-canvas'].width = 1200
+    this.$refs['my-canvas'].height = 600
+    var ctx = this.$refs['my-canvas'].getContext('2d')
+    ctx.moveTo(0, 0)
+    ctx.lineTo(250, 100)
+    ctx.stroke()
   }
 }
 </script>
+<style lang="scss" scoped>
+.canvas {
+  border: 1px solid black;
+}
+</style>
