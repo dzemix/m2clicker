2b36211ea794db16c3edaa917427b6404badc82a
unknown <czesio517619032@wp.pl>
2019-05-21 14:02:32 +0200

    clearup and add scsss

---
+++ src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello {
+  color:red;
+}
+</style>
