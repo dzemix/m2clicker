db79f243836e94c8a7960c2f20c302f0321bc9c8
unknown <czesio517619032@wp.pl>
2019-05-21 14:46:26 +0200

    add canvas

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,23 +1,37 @@
 <template>
-  <div class="hello">
-    <p>hello world</p>
+  <div class="main">
+    <h2>M2 Clicker</h2>
+    <my-canvas class="my-canvas">
+      <my-game></my-game>
+    </my-canvas>
   </div>
 </template>
 
 <script>
+import MyCanvas from './myCanvas.vue'
+import myGame from './myGame.vue'
 export default {
-  name: 'index',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  name: 'app',
+  components: {
+    MyCanvas,
+    myGame
   }
 }
 </script>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.hello {
-  color:red;
+.main {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  padding: 20px;
+  box-sizing: border-box;
+}
+.my-canvas {
+  width: 1200px; height: 600px; border:1px solid black;
 }
 </style>
---
+++ src/components/myGame.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  inject: ['provider'],
+  computed: {
+    ctx () {
+      return this.provider.context
+    }
+  },
+  render () {
+    this.ctx.moveTo(0, 0)
+    this.ctx.lineTo(200, 100)
+    this.ctx.stroke()
+    return null
+  }
+}
+</script>
